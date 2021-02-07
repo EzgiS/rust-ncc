be4636aadf71b4b0bf41a1198754656d04c25c8a
EzgiS <43857780+EzgiS@users.noreply.github.com>
2021-02-07 11:10:04 -0800

    Add files via upload

---
+++ rust-ncc-main-2/src/lib.rs
@@ -0,0 +1,12 @@
+pub mod cell;
+pub mod experiments;
+pub mod interactions;
+pub mod math;
+pub mod parameters;
+pub mod utils;
+pub mod world;
+
+/// Number of vertices per model cell.
+pub const NVERTS: usize = 16;
+/// Default directory where simulation output will be placed.
+pub const DEFAULT_OUTPUT_DIR: &str = "./output";
